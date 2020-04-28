e54a2f0b03c91fc25fa9d9240dffc0d37a3116a0
Prestwick97 <prestwick97@gmail.com>
2020-04-28 08:10:43 -0700

    update potionDetail.js

--- src/components/PotionDetail.js
+++ src/components/PotionDetail.js
@@ -1,23 +1,31 @@
+export default PotionDetail;
+
 import React from "react";
 import PropTypes from "prop-types";
 
-function PotionDetail(props){
+function PotionDetail(props) {
   const { potion, onClickingDelete } = props;
 
   return (
     <React.Fragment>
       <h1>Potion Details</h1>
-      <h3>{potion.effect} - {potion.name}</h3>
-      <p><em>{potion.price}</em></p>
-      <button onClick={()=> onClickingDelete(potion.id) }>Exit Potion</button>
-      <hr/>
+      <h3>
+        {potion.name} - {potion.effect}
+      </h3>
+      <p>
+        <em>{potion.price}</em>
+      </p>
+      <button onClick={props.onClickingEdit}>Update Potion</button>
+      <button onClick={() => onClickingDelete(potion.id)}>Close Potion</button>
+      <hr />
     </React.Fragment>
   );
 }
 
 PotionDetail.propTypes = {
   potion: PropTypes.object,
-  onClickingDelete: PropTypes.func
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func
 };
 
 export default PotionDetail;
